5dcf346de4a1f849bd79c8c0b530b207bb0ea96c
Pavel <pavlin.tarasenko@mail.ru>
2019-04-01 11:41:39 +0300

    Two step

---
+++ Laba1/Laba1/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Product
+    {
+        string Name { get; set; }
+        int Quantity { get; set; }
+        double Price { get; set; }
+
+        public Product()
+        {
+            Name = "";
+            Quantity = 0;
+            Price = 0;
+        }
+
+        //echo 3 parametrs
+        public void Info()
+        {
+            Console.WriteLine("Артибуты товара");
+            Console.WriteLine($"Название товара {Name}");
+            Console.WriteLine($"Название товара {Quantity}");
+            Console.WriteLine($"Название товара {Price}");
+        }
+        
+        public void getInfo(string name, int quantity, double price)
+        {
+            Console.WriteLine("Введите наименование товара");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите количество товара");
+            Quantity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите цену товара");
+            Price = Convert.ToDouble(Console.ReadLine());
+        }
+
+
+    }
+}
--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -10,6 +10,11 @@ namespace Laba1
     {
         static void Main(string[] args)
         {
+           /* Product Vladik = new Product();
+            Vladik.Info();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода из программы");
+            Console.ReadKey();*/
         }
     }
 }
